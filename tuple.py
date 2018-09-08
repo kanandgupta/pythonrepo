9fc836b85beea7ad05c304f0c389573226a76c4f
Puja <kanandgupta@gmail.com>
2018-09-08 21:29:55 +0530

    assignment

---
+++ tuple.py
@@ -0,0 +1,12 @@
+tup1 = ('physics', 'chemistry', 1997, 2000);
+tup2 = (1, 2, 3, 4, 5 );
+tup3 = "abc", 'b', 'c', 'd' ;
+
+print tup1 
+print type (tup1)
+print type (tup3) ,tup3
+print type (tup2),tup2
+print min(tup2)
+print max(tup2)
+
+
